9e137c4526fa5e4b9dda3a64c92d5091d04c105c
freespace8 <f8_soft@qq.com>
2020-12-09 13:19:03 +0800

    Update index.js
     钉钉里会报错

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import VueWordCloud from './VueWordCloud';
 
-{
-	let {window} = globalThis;
-	if (window) {
-		let {Vue} = window;
-		if (Vue) {
-			Vue.component(VueWordCloud.name, VueWordCloud);
-		}
-	}
-}
-
 export default VueWordCloud;
